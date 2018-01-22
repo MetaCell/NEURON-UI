2a5d1a47b44b5ad551e68d317a9d9f99f1aac105
Adrian Quintana <adrianquintana@gmail.com>
2018-01-22 15:10:06 +0000

    remove widgetsnbextension enabling

--- utilities/install.py
+++ utilities/install.py
@@ -41,7 +41,7 @@ subprocess.call(['pip', 'install', '-e', '.'], cwd='../org.geppetto.frontend.jup
 print("Installing jupyter_geppetto Jupyter Extension ...")
 subprocess.call(['jupyter', 'nbextension', 'install', '--py', '--symlink', '--user', 'jupyter_geppetto'], cwd='../org.geppetto.frontend.jupyter')
 subprocess.call(['jupyter', 'nbextension', 'enable', '--py', '--user', 'jupyter_geppetto'], cwd='../org.geppetto.frontend.jupyter')
-subprocess.call(['jupyter', 'nbextension', 'enable', '--py', 'widgetsnbextension'], cwd='../org.geppetto.frontend.jupyter')
+# subprocess.call(['jupyter', 'nbextension', 'enable', '--py', 'widgetsnbextension'], cwd='../org.geppetto.frontend.jupyter')
 subprocess.call(['jupyter', 'serverextension', 'enable', '--py', 'jupyter_geppetto'], cwd='../org.geppetto.frontend.jupyter')
 
 print("Installing neuron_ui python package ...")
